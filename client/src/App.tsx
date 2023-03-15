85c945e034a748492ea4db63eb53bc19665d1694
yasuke <yassine.ksabi.dev.blockchain@gmail.com>
2023-03-15 23:45:08 +0100

    *

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [decks, setDecks] = useState<{ loading: boolean, error: string | false, data: { title: string }[] }>({
+        loading: false,
+        data: [],
+        error: false
+    })
+
+    useEffect(() => {
+        setDecks(prev => ({...prev, loading: true, error: false}))
+        axios.get<{ title: string }[]>(`${process.env.REACT_APP_API_ENDPOINT}/decks`).then(r => {
+            setDecks(prev => ({...prev, data: r.data, loading: false}))
+        }).catch(e => {
+            console.error(e)
+            setDecks(prev => ({...prev, loading: false, error: "An error occurred while fetching decks."}))
+        })
+    }, [])
+
+    return <>
+        {decks.loading && "Loading decks"}
+        {decks.error && decks.error}
+
+        {!decks.loading && !decks.error && <>
+            {decks.data.length > 0 ?
+                <>
+                    <h3>Decks</h3>
+                    <ul>
+                        {decks.data.map((item, index) => (
+                            <li key={index}>{item.title}</li>
+                        ))}
+                    </ul>
+
+                </> :
+                "No deck created yet ..."}
+
+
+        </>}
+
+    </>
 }
 
 export default App;
